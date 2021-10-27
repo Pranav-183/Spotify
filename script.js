063e8cf9418e446f90cb04cc782229993c66660f
Pranav <brindhameera@gmail.com>
2021-10-27 17:24:04 +0530

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('Welcome to Spotify');
 
 //Initialise the variables
 let songIndex = 0;
-let audioElement = new Audio('./songs/1.mp3.mp3');
+let audioElement = new Audio('./songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
